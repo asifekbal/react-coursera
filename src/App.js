ad2f9dbbb4789e0aeca586c6faf550dc75480d9d
Asif Ekbal <asif.ekbal@peoplestrong.com>
2019-06-26 15:40:16 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap';
+import {DISHES} from './shared/dishes'
 import MenuComponent from './components/MenuComponent';
 
-function App() {
-  return (
-        <div>
-          <Navbar dark color="primary">
-            <div className="container">
-                  <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-            </div>
-          </Navbar>
-          <MenuComponent></MenuComponent>
-        </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       dishes : DISHES
+    }
+  }
+  
+  render() {
+    return (
+      <div>
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <MenuComponent dishes={this.state.dishes}></MenuComponent>
+      </div>
+    );
+  }
 }
 
 export default App;
