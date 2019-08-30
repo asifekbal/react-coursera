1d1f4bed77cbe1722d6ce4212fabe08715ae852e
Asif Ekbal <asif.ekbal@peoplestrong.com>
2019-08-30 16:26:48 +0530

    Single Page Applications Part-1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,45 +1,56 @@
 import React, { Component } from 'react';
 import MenuComponent from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import HeaderComponent from './HeaderComponent';
 import FooterComponent from './FooterComponent';
-import {Switch, Route,Redirect} from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import HomeComponent from './HomeComponent';
+import ContactComponent from './ContactComponent';
 
 
 class MainComponent extends Component {
   constructor(props) {
     super(props)
     this.state = {
-       dishes : DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     }
   }
-  
+
   render() {
 
     const HomePage = () => {
       return (
-        <HomeComponent/>
+        <HomeComponent dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       )
     }
-    
+
     return (
       <div>
-        <HeaderComponent/>
-          <Switch>
-            <Route path="/home" component={HomePage} />
-            <Route exact="/menu" component={()=> <MenuComponent dishes={this.state.dishes}/>} />
-            <Redirect to ="/home"/>
-          </Switch>
-        <FooterComponent/>
+        <HeaderComponent />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <MenuComponent dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={ContactComponent} />
+          <Redirect to="/home" />
+        </Switch>
+        <FooterComponent />
       </div>
     );
   }
-  onDishCLickHandler = (dishId)=>{
+  onDishCLickHandler = (dishId) => {
     this.setState({
-        selectedDish : dishId
+      selectedDish: dishId
     })
-}
+  }
 }
 
 export default MainComponent;
