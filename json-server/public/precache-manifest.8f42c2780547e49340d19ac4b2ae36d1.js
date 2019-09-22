9d1206f860ecf6b5cc66087340ed8708e40079ca
Asif Ekbal <asif.sunny678@gmail.com>
2019-09-23 01:25:15 +0530

    JSON SERVER committed in REACT folder. It can be removed without affecting React Application

---
+++ json-server/public/precache-manifest.8f42c2780547e49340d19ac4b2ae36d1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "038ff4bdb2e48026432bc387c2fc785d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82c5bd929a7c7e5273b7",
+    "url": "/static/css/2.1dbf9b60.chunk.css"
+  },
+  {
+    "revision": "a9814847285ac7d385c6",
+    "url": "/static/css/main.0822a069.chunk.css"
+  },
+  {
+    "revision": "82c5bd929a7c7e5273b7",
+    "url": "/static/js/2.385f4d35.chunk.js"
+  },
+  {
+    "revision": "a9814847285ac7d385c6",
+    "url": "/static/js/main.8e74d8fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
